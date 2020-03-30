05e83fb653c69c40e14eb157080d1223772ae8d2
Shankara <=>
2020-03-30 06:38:39 +0530

    Modified loops.c

--- src/loops.c
+++ src/loops.c
@@ -30,14 +30,14 @@ void fibonacci(int no_of_terms) {
   }
 }
 
-void odd_series(int N) {
-  for(int index = 1; index <= N; index += 2) {
+void odd_series(int number) {
+  for(int index = 1; index <= number; index += 2) {
     printf("%d\n", index);
   }
 }
 
-void even_series(int N) {
-  for(int index = 2; index <= N; index += 2) {
+void even_series(int number) {
+  for(int index = 2; index <= number; index += 2) {
     printf("%d\n", index);
   }
 }
@@ -48,15 +48,15 @@ void multiplication_table(int m, int n) {
   }
 }
 
-void print_odd_numbers(int start, int N) {
+void print_odd_numbers(int start, int number) {
   int first_odd = start % 2 == 0 ? start + 1 : start;
-  for(int index = first_odd; index <= N; index += 2) {
+  for(int index = first_odd; index <= number; index += 2) {
     printf("%d\n", index);
   }
 }
 
-void print_nth_numbers(int N, int start, int end) {
-  for(int index = start; index <= end; index += N) {
+void print_nth_numbers(int number, int start, int end) {
+  for(int index = start; index <= end; index += number) {
     printf("%d\n", index);
   }
 }
@@ -70,24 +70,24 @@ long sum_of_even(int start, int end) {
   return sum;
 }
 
-void print_odd_backwards(int N) {
-  int first_odd = N % 2 == 0 ? N - 1 : N;
+void print_odd_backwards(int number) {
+  int first_odd = number % 2 == 0 ? number - 1 : number;
   for(int index = first_odd; index > 0; index -= 2) {
     printf("%d\n", index);
   }
 }
 
-long product_of_N(int start, int N) {
+long product_of_N(int start, int number) {
   int product = 1;
-  for(int index = 0; index < N; index++, start++) {
+  for(int index = 0; index < number; index++, start++) {
     product *= start;
   }
   return product;
 }
 
-long sum_of_N(int start, int N) {
+long sum_of_N(int start, int number) {
   int sum = 0;
-  for(int index = 0; index < N; index++, start++) {
+  for(int index = 0; index < number; index++, start++) {
     sum += start;
   }
   return sum;
@@ -95,45 +95,45 @@ long sum_of_N(int start, int N) {
 
 int main() {
   int choice = 1;
-  int N, start, end, n1, n2;
+  int number, start, end, num1, num2;
   printf("1. factorial\n2. fibonacci\n3. odd_series\n4. even_series\n5. multiplication_table\n6. sum_of_N\n7. product_of_N\n8. print_odd_numbers\n9. print_nth_numbers\n10. sum_of_even\n11. print_odd_backwards\n\n");
   printf("Enter the function number to run: ");
   scanf("%d", &choice);
   switch(choice) {
     case 1:
       printf("Enter the number: ");
-      scanf("%d", &N);
-      printf("Factorial of %d: %ld\n", N, factorial(N));
+      scanf("%d", &number);
+      printf("Factorial of %d: %ld\n", number, factorial(number));
       break;
     case 2:
       printf("Enter the number of terms: ");
-      scanf("%d", &N);
-      fibonacci(N);
+      scanf("%d", &number);
+      fibonacci(number);
       break;
     case 3:
       printf("Enter the number: ");
-      scanf("%d", &N);
-      odd_series(N);
+      scanf("%d", &number);
+      odd_series(number);
       break;
     case 4:
       printf("Enter tha number: ");
-      scanf("%d", &N);
-      even_series(N);
+      scanf("%d", &number);
+      even_series(number);
       break;
     case 5: 
       printf("Enter values of m and n: ");
-      scanf("%d %d", &n1, &n2);
-      multiplication_table(n1, n2);
+      scanf("%d %d", &num1, &num2);
+      multiplication_table(num1, num2);
       break;
     case 6:
-      printf("Enter starting number and N: ");
-      scanf("%d %d", &n1, &n2);
-      printf("Sum of %d numbers from %d: %ld\n", n2, n1, sum_of_N(n1, n2));
+      printf("Enter starting number and number: ");
+      scanf("%d %d", &num1, &num2);
+      printf("Sum of %d numbers from %d: %ld\n", num2, num1, sum_of_N(num1, num2));
       break;
     case 7:
-      printf("Enter starting number and N: ");
-      scanf("%d %d", &n1, &n2);
-      printf("Product of %d numbers from %d: %ld\n", n2, n1, product_of_N(n1, n2));
+      printf("Enter starting number and number: ");
+      scanf("%d %d", &num1, &num2);
+      printf("Product of %d numbers from %d: %ld\n", num2, num1, product_of_N(num1, num2));
       break;
     case 8:
       printf("Enter the start and end numbers: ");
@@ -141,9 +141,9 @@ int main() {
       print_odd_numbers(start, end);
       break;
     case 9:
-      printf("Enter the N, start and end numbers: ");
-      scanf("%d %d %d", &N, &start, &end);
-      print_nth_numbers(N, start, end);
+      printf("Enter the number, start and end numbers: ");
+      scanf("%d %d %d", &number, &start, &end);
+      print_nth_numbers(number, start, end);
       break;
     case 10:
       printf("Enter the start and end numbers: ");
@@ -152,8 +152,8 @@ int main() {
       break;
     case 11:
       printf("Enter the number: ");
-      scanf("%d", &N);
-      print_odd_backwards(N);
+      scanf("%d", &number);
+      print_odd_backwards(number);
       break;
     default:
       printf("Invalid choice\n");
